72094741efbcea5ac6d7b9beaafe0dce84353b59
Nome <miranhaomi@gmail.com>
2023-03-02 18:15:31 -0300

    colocando o index.js dentro da pasta src

---
+++ server/src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors =  require('cors');
+const morgan = require('morgan');
+
+// instanciando o prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// aqui vai ser importado as rotas
+
+// instanciando o express
+const app = express();
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// rotas aqui
+
+const port =  process.env.PORT || 3333;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
