1fa2ff9a7a46cc47c301ff896767bb796918aa18
Nome <miranhaomi@gmail.com>
2023-03-02 17:48:05 -0300

    importando o prisma no index.js

--- server/index.js
+++ server/index.js
@@ -2,3 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors =  require('cors');
 const morgan = require('morgan');
+
+// instanciando o prisma
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+// aqui vai ser importado as rotas
