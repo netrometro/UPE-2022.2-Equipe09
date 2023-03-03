8147d4c2a24c4e2ea752de79daaec81f67af5e7c
Nome <miranhaomi@gmail.com>
2023-03-02 21:14:46 -0300

    instanciando o prisma e o express no userController.js

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -3,4 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-   
+
+require('dotenv').config();
+const app =  express()
+
+const {PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
