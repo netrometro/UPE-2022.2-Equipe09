2db98af5dfc6a651b77bc1c4a3d82e42c53e1860
Nome <miranhaomi@gmail.com>
2023-03-02 21:59:18 -0300

    criando a rota de registrar o usuário, usando um try catch, caso funcione, o sistema vai retornar uma mensagem avisando que o usuário foi criado. Porém, caso não ocorra, vai retornar um erro

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -14,3 +14,29 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(cors());
 
+// registrar o usuário
+const registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword
+            }
+        });
+        if(user) {
+            return res.json({message: 'usuário cirado com sucesso!!!'})
+        }
+
+        const token = jwt.sign({ userId: user.id}, process.env.JWT_SECRET);
+
+        res.json({user, token});
+    }   catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'erro ao registrar usuário'})
+    }
+};
