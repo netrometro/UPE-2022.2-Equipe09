c6433cabda3a56e6b93800ded4b9ab975c575932
Nome <miranhaomi@gmail.com>
2023-03-05 14:52:38 -0300

    Função Login
    aqui vai retornar um JSX que vai renderizar uma formulário de login com campos de email e senha

--- web/app/src/components/Login/Login.js
+++ web/app/src/components/Login/Login.js
@@ -2,3 +2,9 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import '../../style.css';
 import { Link } from 'react-router-dom';
+
+export function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+}
