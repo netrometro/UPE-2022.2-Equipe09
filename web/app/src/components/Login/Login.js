839b85c74c407876b13e735fb83d2606bb780574
Nome <miranhaomi@gmail.com>
2023-03-05 14:57:40 -0300

    Método handleSubmit da função Login
    o método handleSubmit é chamado quando o form é enviado, impedindo o comportamento padrão do form usando o 'preventDefault()' e faz uma solicitação post para a api no http informado, usando a biblioteca axios

--- web/app/src/components/Login/Login.js
+++ web/app/src/components/Login/Login.js
@@ -7,4 +7,19 @@ export function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:5000/api/users/login', {
+                email,
+                password
+            });
+
+            setMessage('Logado com sucesso');
+        }   catch(error) {
+            console.log(error);
+            setMessage('Erro ao efetuar login');
+        }
+    };
 }
