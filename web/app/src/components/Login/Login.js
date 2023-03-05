c4121b95b0c276e61e12f22021d76bd407de9ba4
Nome <miranhaomi@gmail.com>
2023-03-05 16:17:38 -0300

    Função Register
    a função register retorna um JSX e renderiza um form de registro com campos para o nome, e-mail e senha

--- web/app/src/components/Login/Login.js
+++ web/app/src/components/Login/Login.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import '../../style.css';
-import { Link } from 'react-router-dom';
+/* import '../../style.css'; */
 
 export function Login() {
     const [email, setEmail] = useState('');
---
+++ web/app/src/components/Register/Register.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+/* import '../../style.css'; */
+
+export function Register() {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] =  useState('');
+
+    
+}
