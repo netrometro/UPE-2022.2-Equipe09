03c6b0050beea00cc8d6d25e2b16186e20cae875
Nome <miranhaomi@gmail.com>
2023-03-09 16:34:25 -0300

    Atualizando arquivos no web
    Apenas atualizando alguns arquivos, nada de mais, colocando tocs que não vão afetar o sistema

--- web/app/src/components/Login/Login.js
+++ web/app/src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import '../../SocialMediaStyle/Style.css';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export function Login() {
     const [email, setEmail] = useState('');
@@ -46,3 +46,4 @@ export function Login() {
         </div>
     );
 };
+
