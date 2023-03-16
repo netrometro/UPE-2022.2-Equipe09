88ff797ea3187c640b82aaf6eea7440aa5906fdd
Nome <miranhaomi@gmail.com>
2023-03-16 15:32:36 -0300

    Criando o useEffect da função MeProfile
    O useEffect a seguir está sendo criado para pegar o get do back-end usando o axios do http da função getMe que está no back-end, a qual vai retornar as informações do usuário do seu próprio perfil.

--- web/app/src/components/MeProfile/MeProfile.js
+++ web/app/src/components/MeProfile/MeProfile.js
@@ -5,3 +5,20 @@ import { Link, useParams } from "react-router-dom";
 export function MeProfile() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await axios.get("http://localhost:3005/api/users/me", {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        setUser(res.data.user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUser();
+  }, []);
+
