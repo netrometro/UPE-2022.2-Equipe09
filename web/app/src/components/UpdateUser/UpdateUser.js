99f8e4d5aa6cecddd596fc5132e95b6141570c74
Nome <miranhaomi@gmail.com>
2023-03-16 17:05:19 -0300

    Função UpdateuUser
    Criando a função UpdateUser que vai receber os campos mostrados e retornar mensagens de sucesso ou erro.

--- web/app/src/components/UpdateUser/UpdateUser.js
+++ web/app/src/components/UpdateUser/UpdateUser.js
@@ -3,3 +3,21 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import '../../SocialMediaStyle/UptadeUser.css';
 
+export function UpdateUser() {
+  const [userData, setUserData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    profileimageURL: "",
+  });
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
