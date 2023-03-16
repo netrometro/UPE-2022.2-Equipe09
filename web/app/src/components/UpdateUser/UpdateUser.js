2dc956bc9642d3951e12e7748fb70d2deefa0ddf
Nome <miranhaomi@gmail.com>
2023-03-16 17:49:48 -0300

    Criando o UpdateUser.css
    Criando o arquivo UpdateUser.css que vai ser responsável por estilizar a parte de atualização do perfil do usuário. Foi simples fazer, já que segue o mesmo padrão do de Login e Register.

--- web/app/src/components/UpdateUser/UpdateUser.js
+++ web/app/src/components/UpdateUser/UpdateUser.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import '../../SocialMediaStyle/UptadeUser.css';
+import '../../SocialMediaStyle/UpdateUser.css';
 
 export function UpdateUser() {
   const [userData, setUserData] = useState({
