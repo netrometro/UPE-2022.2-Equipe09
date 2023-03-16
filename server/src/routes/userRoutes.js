f9775a99297b53ff1e450fb4977f8a896bd386de
Nome <miranhaomi@gmail.com>
2023-03-16 11:00:37 -0300

    Rotas http atualizadas do userController.js
    Adicionando as rotas http para as funções getMe e UpdateUser do userController.js

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const router =  express.Router();
+const { verifyToken } = require('../middleware/authMiddleware');
 
 const {
     registerUser,
     loginUser,
+    updateUser,
+    getMe,
 } = require('../controllers/userController');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.put('/updateprofile', verifyToken, updateUser);
+router.get('/me', verifyToken, getMe);
 
 module.exports = router;
