2f9b7b8c3cbff635ed4be04233fd8f4b8259e7be
Nome <miranhaomi@gmail.com>
2023-03-16 17:08:47 -0300

    handleSubmit no UpdateUser
    criando a função assincrona handleSubmit que vai receber os campos já mudados pelo o usuário e vai fazer um put usando o axios para o http que é mostrado. Caso o Update esteja de acordo, vai retornar uma mensagem para o usuário e agora ele vai poder retornar ao perfil dele, mas caso não funcione, uma mensagem de erro vai ser retornada.

--- web/app/src/components/UpdateUser/UpdateUser.js
+++ web/app/src/components/UpdateUser/UpdateUser.js
@@ -21,3 +21,24 @@ export function UpdateUser() {
     }));
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.put(
+        "http://localhost:3002/api/users/updateprofile",
+        userData,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        }
+      );
+      setSuccessMessage("Perfil atualizado com sucesso! Agora você pode retornar ao seu perfil");
+      setErrorMessage("");
+    } catch (error) {
+      console.log(error);
+      setSuccessMessage("");
+      setErrorMessage("Falha ao atualizar perfil.");
+    }
+  };
+
