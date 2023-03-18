02b94a760b8ede856be886154dfd70633402674b
iohanasalvador <109769766+iohanasalvador@users.noreply.github.com>
2023-03-17 22:22:59 -0300

    Criando a função getAllPosts
    criando, dentro do postController.js, a função getAllPosts que vai pegar todos os posts criados e retornar ao usuário, sendo assim, um feed.

--- server/src/controllers/postController.js
+++ server/src/controllers/postController.js
@@ -19,3 +19,30 @@ const createPost = async (req, res) => {
     res.status(500).json({ error: 'Erro ao criar post' });
   }
 }
+
+const getAllPosts = async (req, res) => {
+    try {
+        const posts = await prisma.post.findMany({
+            select: {
+                user: {
+                    select: {
+                        username: true,
+                        email: true
+                    },
+                },
+                caption: true,
+                imageURL: true,
+                createAt: true,
+            },
+        });
+        res.status(200).json(posts);
+    }   catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'erro ao buscar posts'});
+    }
+};
+
+module.exports = {
+  createPost,
+  getAllPosts,
+};
