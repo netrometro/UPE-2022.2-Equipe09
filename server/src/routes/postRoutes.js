71893c0aabc1ca6f373b5337e9251ce89115a54f
Nome <miranhaomi@gmail.com>
2023-03-26 12:49:33 -0300

    Atualizando o postRoutes
    Atualizando o postRoutes para adicionar a rota get que vai retornar para o usuário a postagem desejada pelo id por meio da requisição get

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -6,11 +6,13 @@ const {
     createPost,
     getAllPosts,
     getMyPosts,
+    getPostById,
 } = require('../controllers/postController');
 
 router.post('/create', verifyToken, createPost);
 router.get('/getAll', verifyToken, getAllPosts);
 router.get('/myPosts', verifyToken, getMyPosts);
+router.get('/seePost/:id', verifyToken, getPostById);
 
 module.exports = router;
 
