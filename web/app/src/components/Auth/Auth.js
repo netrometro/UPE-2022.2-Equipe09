9b5c8586e903dea8f1c0a3e80060232295f7b197
Nome <miranhaomi@gmail.com>
2023-03-26 12:55:20 -0300

    Atualizando o Auth.js

--- web/app/src/components/Auth/Auth.js
+++ web/app/src/components/Auth/Auth.js
@@ -1,13 +1,12 @@
 export const login = (token) => {
-    localStorage.setItem("token", token);
-  };
-  
-  export const logout = () => {
-    localStorage.removeItem("token");
-  };
-  
-  export const isAuthenticated = () => {
-    return localStorage.getItem("token") !== null;
-  };
+  localStorage.setItem("token", token);
+};
 
-  
+export const logout = () => {
+  localStorage.removeItem("token");
+};
+
+export const isAuthenticated = () => {
+  return localStorage.getItem("token") !== null;
+};
+
