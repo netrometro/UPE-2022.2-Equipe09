939f57853b18200218781a55f681c626540b4d0d
Nome <miranhaomi@gmail.com>
2023-03-26 12:52:43 -0300

    Atualizando o postRoutes.js
    Adicionando a rota put que vai fazer uma requisição de put para atualizar a postagem desejada pelo o usuário.

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -7,12 +7,14 @@ const {
     getAllPosts,
     getMyPosts,
     getPostById,
+    updatePost
 } = require('../controllers/postController');
 
 router.post('/create', verifyToken, createPost);
 router.get('/getAll', verifyToken, getAllPosts);
 router.get('/myPosts', verifyToken, getMyPosts);
 router.get('/seePost/:id', verifyToken, getPostById);
+router.put('/update/:id', verifyToken, updatePost);
 
 module.exports = router;
 
