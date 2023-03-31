99ec5908260d2e9bcb9b12eaa721c4222cc850c3
Nome <miranhaomi@gmail.com>
2023-03-31 15:18:28 -0300

    Atualizando o postRoutes.js
    como o comentário faz parte da postagem, dentro do postRoutes.js foi adicionado a constante que vai importar as funções do commentController.js e também foi adicionando as rotas post e get que são necessárias para o funcionamento das funções.

--- server/src/routes/postRoutes.js
+++ server/src/routes/postRoutes.js
@@ -10,11 +10,18 @@ const {
     updatePost
 } = require('../controllers/postController');
 
+const {
+    addComment,
+    getAllComments,
+} = require('../controllers/commentController');
+
 router.post('/create', verifyToken, createPost);
 router.get('/getAll', verifyToken, getAllPosts);
 router.get('/myPosts', verifyToken, getMyPosts);
 router.get('/seePost/:id', verifyToken, getPostById);
 router.put('/update/:id', verifyToken, updatePost);
+router.post('/seePost/:id/comment', verifyToken, addComment);
+router.get('/seePost/:id/comments', verifyToken, getAllComments);
 
 module.exports = router;
 
