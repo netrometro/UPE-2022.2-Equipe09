093f6da22fed1232e386bf47e6930747f9166f3a
Nome <miranhaomi@gmail.com>
2023-04-07 16:41:02 -0300

    Atualizando o searchUserRoutes.js
    Atualizando o searchUserRoutes.js para agora ele receber o searchUserByUsername

--- server/src/routes/searchUserRoutes.js
+++ server/src/routes/searchUserRoutes.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const { verifyToken } = require('../middleware/authMiddleware');
 
 const {
-    searchUserByEmail,
+    searchUserByUsername,
     getUserInfo
 } = require('../controllers/searchUserController');
 
-router.get('/:email', verifyToken, searchUserByEmail);
+router.get('/:username', verifyToken, searchUserByUsername);
 router.get('/profile/:id', verifyToken, getUserInfo);
 
 module.exports = router;
